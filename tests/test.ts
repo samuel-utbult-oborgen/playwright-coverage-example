b7706f277969a39fdfb873e7fd736b4a14b0a993
Samuel Utbult <samuel.utbult@oborgen.se>
2023-02-20 09:12:32 +0100

    Add a basic project that displays the problem with __coverage__

---
+++ tests/test.ts
@@ -0,0 +1,8 @@
+// This is displayed as a syntax error in VS Code but compilation works just
+// fine. However, removing ".ts" causes a compilation error.
+import { expect, test } from './baseFixtures.ts';
+
+test('index page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+});
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import istanbul from 'vite-plugin-istanbul';
+
+export default defineConfig({
+	build: {
+		sourcemap: "hidden",
+	},
+	plugins: [
+		sveltekit(),
+		istanbul({
+			include: 'src/*',
+			exclude: ['node_modules', 'test/'],
+			extension: [ '.js', '.ts', '.vue' ],
+			requireEnv: true,
+		  }),
+	],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
